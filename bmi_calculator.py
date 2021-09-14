3cb389a104f8ba6197f11568dd140ead571c4e8e
Júlio Billet <76001545+juliobillet@users.noreply.github.com>
2021-09-14 09:17:46 -0300

    Create bmi_calculator.py

---
+++ bmi_calculator.py
@@ -0,0 +1,5 @@
+# Study Test - BMI Calculator
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+print("Your BMI is:", int(float(weight) / ((float(height) ** 2))))
